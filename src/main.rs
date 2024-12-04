5bcc7f865527ac54f04427016bd52aa269d703c1
reya <reya@lume.nu>
2024-12-04 08:35:51 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -20,24 +20,26 @@ async fn main() -> Result<()> {
     let public_key =
         PublicKey::from_bech32("npub1zfss807aer0j26mwp2la0ume0jqde3823rmu97ra6sgyyg956e0s6xw445")?;
 
-    // Fetch user's metadata
-    let _metadata = client.fetch_metadata(public_key, None).await?;
-
     let filter = Filter::new()
         .kind(Kind::Metadata)
         .author(public_key)
         .limit(1);
-    let events = client.database().query(vec![filter]).await?;
-    let event = events.first().unwrap();
-    let seens = client
-        .database()
-        .event_seen_on_relays(&event.id)
-        .await?
-        .unwrap();
 
-    for url in seens {
-        println!("Seen on: {}", url)
-    }
+    client
+        .handle_notifications(|notification| async {
+            if let RelayPoolNotification::Event { event, .. } = notification {
+                println!("Event: {}", event.as_json())
+            }
+            Ok(false)
+        })
+        .await?;
+
+    _ = tokio::spawn(async move {
+        if let Ok(output) = client.subscribe(vec![filter], None).await {
+            println!("Output: {:?}", output);
+        }
+    })
+    .await;
 
     Ok(())
 }
